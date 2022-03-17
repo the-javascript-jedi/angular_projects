400306bc20344e114999c32c847e3df3155b4da4
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-03-17 10:34:32 +0530

    added child component

--- basics/src/app/app.module.ts
+++ basics/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 // Import AppComponent
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 
 // We need to register  the component with our module, we can register components by adding an array to the decorator called declarations.
 // By registering the component, Angular will allow us to use the components the browser will be able to understand how to render the component.
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
