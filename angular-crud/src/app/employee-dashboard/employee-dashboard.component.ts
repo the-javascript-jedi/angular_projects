46db1af6e9b73718cd5f9a0ceb4e182594fb00b7
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-03-21 13:31:13 +0530

    added insert employee

--- angular-crud/src/app/employee-dashboard/employee-dashboard.component.ts
+++ angular-crud/src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup} from '@angular/forms';
+import { EmployeeModel } from '../Models/employee-dashboard.model';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-employee-dashboard',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeDashboardComponent implements OnInit {
 
-  constructor() { }
+  formValue!:FormGroup;
+  constructor(private formBuilder:FormBuilder,private _apiService:ApiService) { }
+  // create an object of type Employee Model
+  employeeModelObj:EmployeeModel=new EmployeeModel();
 
   ngOnInit(): void {
+    this.formValue=this.formBuilder.group({
+      firstName:[''],
+      lastName:[''],
+      email:[''],
+      mobile:[''],
+      salary:['']
+    })
   }
-
+  // PostEmployee details
+  postEmployeeDetails(){
+    this.employeeModelObj.firstName=this.formValue.value.firstName;
+    this.employeeModelObj.lastName=this.formValue.value.lastName;
+    this.employeeModelObj.email=this.formValue.value.email;
+    this.employeeModelObj.mobile=this.formValue.value.mobile;
+    this.employeeModelObj.salary=this.formValue.value.salary;
+    // Make APi request
+    this._apiService.postEmployeeService(this.employeeModelObj).subscribe(postResponse=>{
+      console.log("postResponse",postResponse);
+      alert("Employee Added Successfully");
+      this.formValue.reset();
+      // close the modal after successfully submitting the form
+      let refClose=document.getElementById('closeBtn');
+      // Get a reference of Bootstrap cancel button and trigger the click event
+      refClose!.click();
+    },
+    err=>{
+      console.log("err",err);
+      alert("Something went wrong");
+    }
+    )
+  }
 }
---
+++ angular-crud/src/app/service/api.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private _http:HttpClient) { }
+  // POST
+  postEmployeeService(data:any){
+    return this._http.post<any>("http://localhost:3000/posts",data)
+    .pipe(map((response:any)=>{
+      return response;
+    }))
+  }
+  // GET
+  getEmployeeService(){
+    return this._http.get<any>("http://localhost:3000/posts")
+    .pipe(map((response:any)=>{
+      return response;
+    }))
+  }
+  // UPDATE/PUT
+  updateEmployeeService(data:any,id:number){
+    return this._http.put<any>("http://localhost:3000/posts"+id,data)
+    .pipe(map((response:any)=>{
+      return response;
+    }))
+  }
+  // DELETE
+  deleteEmployeeService(id:number){
+    return this._http.delete<any>("http://localhost:3000/posts"+id)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+}
