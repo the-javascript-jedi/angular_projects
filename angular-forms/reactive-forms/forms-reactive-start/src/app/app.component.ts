859a227eee4cadc259182599bfaf2473c38bd3b7
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-03-29 09:44:34 +0530

    added form array

--- angular-forms/reactive-forms/forms-reactive-start/src/app/app.component.ts
+++ angular-forms/reactive-forms/forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup,Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup,Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -18,10 +18,24 @@ export class AppComponent implements OnInit {
         'emailFormControl':new FormControl(null,[Validators.required,Validators.email]),
       }),
       'genderFormControl':new FormControl("male"),
+      'hobbiesFormArray':new FormArray([])
     });
   }
   // step-4 - display the signupFormGroup
   onSubmit(){
     console.log("this.signupFormGroup",this.signupFormGroup);
+  }
+  //Adding Form Array 
+  onAddHobby(){
+    // specify a FormControl
+    const customControl=new FormControl(null,Validators.required);
+    // for FormArray we need to do typecasting
+    (<FormArray>this.signupFormGroup.get('hobbiesFormArray')).push(customControl);
+  }
+  // Delete Hobby
+  onDeleteHobby(idToDelete){
+    console.log("idToDelete",idToDelete)
+    console.log("this.signupFormGroup.get('hobbiesFormArray'))",this.signupFormGroup.get('hobbiesFormArray'));
+    (<FormArray>this.signupFormGroup.get('hobbiesFormArray')).controls.splice(idToDelete,1);
   }
 }
