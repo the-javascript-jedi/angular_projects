77d51cb957686361819c21fd3bd632232ab6fe40
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-03-29 22:45:34 +0530

    added http reequests in services

--- angular-http/http-01-start/src/app/app.component.ts
+++ angular-http/http-01-start/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators';
 import { IPost } from 'src/interfaces/post.model';
+import { PostsService } from './posts.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,56 +11,31 @@ import { IPost } from 'src/interfaces/post.model';
 export class AppComponent implements OnInit {
   loadedPosts:IPost[] = [];
   isFetching=false;
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,private postsService:PostsService) {}
 
-  ngOnInit() {
-    this.fetchPosts();
-  }
-
-  onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    console.log(postData);
-    // Firebase
-    // posts.json added for firebase
-    // postData must be sent as JSON data
-    //request must be binded using observable
-    this.http.post<{name:string}>('https://ng-complete-guide-843b5-default-rtdb.firebaseio.com/posts.json',{
-      postData
-    }).subscribe(responseData=>{
-      console.log("responseData",responseData);
+  ngOnInit() {    
+    this.isFetching=true;
+    // Make API request
+    this.postsService.fetchPosts().subscribe((posts)=>{
+      this.isFetching=false;
+      this.loadedPosts=posts;
     })
   }
 
+  onCreatePost(postData: { title: string; content: string }) {
+    this.postsService.createAndStore(postData.title,postData.content);   
+  }
+
   onFetchPosts() {
-    // Send Http request
-   this.fetchPosts();
+     this.isFetching=true;
+    // Make API request
+    this.postsService.fetchPosts().subscribe((posts)=>{
+      this.isFetching=false;
+      this.loadedPosts=posts;
+    })
   }
 
   onClearPosts() {
     // Send Http request
-  }
-
-  private fetchPosts(){
-    this.isFetching=true;
-      this.http
-     .get<{[key:string]:IPost}>('https://ng-complete-guide-843b5-default-rtdb.firebaseio.com/posts.json')
-     // responseDataType - putting data in angled brackets is similar to adding in function body (responseData:{[key:string]:IPost})
-     .pipe(map((responseData:{[key:string]:IPost})=>{
-       console.log("responseData",responseData);
-      //  loop through object and create an array
-      const postsArray:IPost[]=[];
-      for(const key in responseData){
-        console.log("key")
-        if(responseData.hasOwnProperty(key)){
-          postsArray.push({...responseData[key],id:key})
-        }
-      }
-      return postsArray;
-     }))
-     .subscribe(posts=>{
-      console.log("posts",posts);
-      this.loadedPosts=posts;
-    })
-     this.isFetching=false;
   }
 }
---
+++ angular-http/http-01-start/src/app/posts.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IPost } from 'src/interfaces/post.model';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor(private http:HttpClient) { }
+  // POST request
+  createAndStore(title:string,content:string){
+    const postData:IPost={title:title,content:content}
+    console.log("postData",postData);
+     // Send Http request
+    // Firebase
+    // posts.json added for firebase
+    // postData must be sent as JSON data
+    //request must be binded using observable
+    this.http.post<{name:string}>('https://ng-complete-guide-843b5-default-rtdb.firebaseio.com/posts.json',{
+      postData
+    }).subscribe(responseData=>{
+      console.log("responseData",responseData);
+    })
+  }
+  // GET Request
+  fetchPosts(){
+    // returns as an observable
+    return this.http
+     .get<{[key:string]:IPost}>('https://ng-complete-guide-843b5-default-rtdb.firebaseio.com/posts.json')
+     // responseDataType - putting data in angled brackets is similar to adding in function body (responseData:{[key:string]:IPost})
+     .pipe(map((responseData:{[key:string]:IPost})=>{
+       console.log("responseData",responseData);
+      //  loop through object and create an array
+      const postsArray:IPost[]=[];
+      for(const key in responseData){
+        console.log("key")
+        if(responseData.hasOwnProperty(key)){
+          postsArray.push({...responseData[key],id:key})
+        }
+      }
+      return postsArray;
+     }))    
+  }
+}
