08015a3d397500cd01c5be3334c70fa490aea46c
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-03-29 09:58:12 +0530

    added custom validator

--- angular-forms/reactive-forms/forms-reactive-start/src/app/app.component.ts
+++ angular-forms/reactive-forms/forms-reactive-start/src/app/app.component.ts
@@ -9,12 +9,15 @@ export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   // step 1 - declare a formGroup
   signupFormGroup:FormGroup;
+  // declare forbiddenusernames array
+  forbiddenUserNames=["chris","anna"];
+
   ngOnInit(): void {
     //step 2 initialize reactive form before rendering it
     // after initializing a reactive form we need to add FormControl which specify the form elements
     this.signupFormGroup=new FormGroup({
       'userDataFormGroup':new FormGroup({
-        'usernameFormControl':new FormControl(null,Validators.required),
+        'usernameFormControl':new FormControl(null,[Validators.required,this.forbiddenNames.bind(this)]),
         'emailFormControl':new FormControl(null,[Validators.required,Validators.email]),
       }),
       'genderFormControl':new FormControl("male"),
@@ -37,5 +40,15 @@ export class AppComponent implements OnInit {
     console.log("idToDelete",idToDelete)
     console.log("this.signupFormGroup.get('hobbiesFormArray'))",this.signupFormGroup.get('hobbiesFormArray'));
     (<FormArray>this.signupFormGroup.get('hobbiesFormArray')).controls.splice(idToDelete,1);
+  }
+  // Custom Validator
+  // return type (key value pair) - {nameIsForbidden:true}
+  forbiddenNames(control:FormControl):{[s:string]:boolean}{
+    // check if the Form control value name is present in forbidden names array
+    if(this.forbiddenUserNames.indexOf(control.value)!==-1){
+      return {'nameIsForbidden':true};
+    }
+    // succesfull validation must return null
+    return null;
   }
 }
