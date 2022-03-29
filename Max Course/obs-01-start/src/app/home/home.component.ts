71bcb10e563af4ace93e4c274eb28846c10c2ecb
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-03-30 00:03:51 +0530

    added observable next complete and observable error handling

--- Max Course/obs-01-start/src/app/home/home.component.ts
+++ Max Course/obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,8 +10,39 @@ export class HomeComponent implements OnInit,OnDestroy {
   private firstObservable:Subscription;
   constructor() { }
   ngOnInit() {
-    this.firstObservable=interval(1000).subscribe(count=>{
-      console.log('count',count);
+    // this.firstObservable=interval(1000).subscribe(count=>{
+    //   console.log('count',count);
+    // })
+    const customIntervalObservable=Observable.create(observer=>{
+      let count=0;
+      setInterval(()=>{
+        // .next()-to emit a new value
+        //.error() - throw error
+        //.complete() - to tell we are done
+          observer.next(count);
+          // complete the observable
+          if(count===2){
+            observer.complete();
+          }
+          // simulate an error
+          if(count>3){
+            // throwing an error will cancel the observable, but not comoplete
+            observer.error(new Error('Count is greater 3!'));
+          }
+          count++;
+      },1000)
+    })
+    // Call the observale
+    this.firstObservable=customIntervalObservable.subscribe(data=>{
+      console.log("data",data);
+    },error=>{
+      // handling the error
+      console.log("error",error);
+      alert(error.message);
+    },()=>{
+      // handling the completion of the observable
+      // this will not fire if observable throws error
+      console.log('Completed');
     })
   }
   ngOnDestroy(): void {
