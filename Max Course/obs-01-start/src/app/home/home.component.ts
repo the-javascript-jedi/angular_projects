59f12a7455178e8b0357ca1e18674b95ffdf0757
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-03-29 23:14:30 +0530

    basic observale

--- Max Course/obs-01-start/src/app/home/home.component.ts
+++ Max Course/obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent implements OnInit,OnDestroy {
+  // create a Subscription data to store and  destroy the observable 
+  private firstObservable:Subscription;
   constructor() { }
-
   ngOnInit() {
+    this.firstObservable=interval(1000).subscribe(count=>{
+      console.log('count',count);
+    })
   }
-
+  ngOnDestroy(): void {
+    // whenever we leave that component, we clear that subscription and therefore we prevent memory leaks because we're not keeping old subscriptions around.
+    this.firstObservable.unsubscribe();
+  }
 }
