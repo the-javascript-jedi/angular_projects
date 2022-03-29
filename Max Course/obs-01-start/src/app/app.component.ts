2899825bf340f59f6bec496785e89f47d33ccd7f
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-03-30 00:41:24 +0530

    added subject

--- Max Course/obs-01-start/src/app/app.component.ts
+++ Max Course/obs-01-start/src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,OnDestroy} from '@angular/core';
 import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit,OnDestroy {
   // initialize the data as false
   userActivated=false;
+  // initialize a subscription
+  private activatedSub:Subscription;
   constructor(private userService:UserService) {}
   ngOnInit() {
     // subscribe to the event emitter
-    this.userService.activatedEmitter.subscribe(didActivate=>{
+    this.activatedSub=this.userService.activatedEmitter.subscribe(didActivate=>{
       this.userActivated=didActivate;
     })
+  }
+  ngOnDestroy(): void {
+    // unsubscribe from the observable
+    this.activatedSub.unsubscribe();
   }
 }
