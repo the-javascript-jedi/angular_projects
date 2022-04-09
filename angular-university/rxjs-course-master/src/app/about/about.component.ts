ed1616a1b2c9163b658bd1c9589767a3b9b3817e
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-04-10 00:00:19 +0530

    Initial COmmit

---
+++ angular-university/rxjs-course-master/src/app/about/about.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+    concat,
+    fromEvent,
+    interval,
+    noop,
+    observable,
+    Observable,
+    of,
+    timer,
+    merge,
+    Subject,
+    BehaviorSubject,
+    AsyncSubject,
+    ReplaySubject
+} from 'rxjs';
+import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
+import {createHttpObservable} from '../common/util';
+
+
+@Component({
+    selector: 'about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+    ngOnInit() {
+        // console.log clicks
+        // const click$=fromEvent(document,'click');
+        // click$.subscribe(evt=>console.log("evt",evt),err=>console.log(err),()=>console.log("completed"))
+
+        
+    }
+
+
+}
+
+
+
+
+
+
