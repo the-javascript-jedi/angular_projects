dd9c1ea61a80d6fd3d54a96f6b35b7bcfb54033a
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-04-10 14:01:15 +0530

    initial commit

---
+++ angular-university/rxjs-course-1-operators/src/app/common/util.ts
@@ -0,0 +1,15 @@
+import {Observable} from 'rxjs';
+export function createHttpObservable(url:string) {
+    return Observable.create(observer => {
+          fetch("/api/courses").then(response=>{
+                return response.json();
+            }).then(body=>{
+                observer.next(body);
+                observer.complete();
+            }).catch(err=>{
+                observer.console.error(err);
+                
+            })
+    })
+}
+
