29124cf24c82d9839d14281ac29a114f68934018
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-04-14 21:35:58 +0530

    added search

--- angular-university/rxjs-course-1-operators/src/app/common/util.ts
+++ angular-university/rxjs-course-1-operators/src/app/common/util.ts
@@ -1,15 +1,13 @@
 import {Observable} from 'rxjs';
 export function createHttpObservable(url:string) {
     return Observable.create(observer => {
-          fetch("/api/courses").then(response=>{
+          fetch(url).then(response=>{
                 return response.json();
             }).then(body=>{
                 observer.next(body);
                 observer.complete();
             }).catch(err=>{
-                observer.console.error(err);
-                
+                observer.console.error(err);                
             })
     })
 }
-
