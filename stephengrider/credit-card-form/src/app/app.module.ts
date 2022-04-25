d242695958b731219cb60366b6d5e81c89c69949
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-04-25 23:25:11 +0530

    added form validation

---
+++ stephengrider/credit-card-form/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CardFormComponent } from './components/card-form/card-form.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
