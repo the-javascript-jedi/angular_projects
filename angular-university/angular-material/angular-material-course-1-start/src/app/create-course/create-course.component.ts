ad3f6159cc4dd5e5b2b2883d25a6b8aa309d00c6
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-05-23 23:26:08 +0530

    added stepper

--- angular-university/angular-material/angular-material-course-1-start/src/app/create-course/create-course.component.ts
+++ angular-university/angular-material/angular-material-course-1-start/src/app/create-course/create-course.component.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
-
-
 @Component({
   selector: "create-course",
   styleUrls: ["create-course.component.scss"],
