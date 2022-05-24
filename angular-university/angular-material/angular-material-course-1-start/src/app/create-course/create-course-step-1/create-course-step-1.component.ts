114163445458e1099b6819bea0005356e37fc4d2
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-05-24 22:38:27 +0530

    material form controls

--- angular-university/angular-material/angular-material-course-1-start/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ angular-university/angular-material/angular-material-course-1-start/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,14 +1,11 @@
 import {Component} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
-
-
 @Component({
   selector: "create-course-step-1",
   templateUrl:"create-course-step-1.component.html",
   styleUrls: ["create-course-step-1.component.scss"]
 })
 export class CreateCourseStep1Component {
-
   form = this.fb.group({
     title: ['', [
       Validators.required,
@@ -22,12 +19,8 @@ export class CreateCourseStep1Component {
     longDescription: ['', [Validators.required, Validators.minLength(3)]]
   });
 
-  constructor(private fb: FormBuilder) {
-
-  }
-
+  constructor(private fb: FormBuilder) { }
   get courseTitle() {
     return this.form.controls['title'];
   }
-
 }
