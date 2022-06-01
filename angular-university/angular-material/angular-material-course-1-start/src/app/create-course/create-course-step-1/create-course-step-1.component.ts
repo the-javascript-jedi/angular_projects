86fbd579bc341935624f5615a912520c304d397f
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-06-01 23:57:51 +0530

    added textarea

--- angular-university/angular-material/angular-material-course-1-start/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ angular-university/angular-material/angular-material-course-1-start/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
+const SAMPLE_TEXT="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium harum accusamus commodi mollitia earum architecto voluptatum enim, omnis quidem eligendi.";
 @Component({
   selector: "create-course-step-1",
   templateUrl:"create-course-step-1.component.html",
@@ -16,7 +17,7 @@ export class CreateCourseStep1Component {
     category: ['BEGINNER', Validators.required],
     courseType: ['premium', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+    longDescription: [SAMPLE_TEXT, [Validators.required, Validators.minLength(3)]]
   });
 
   constructor(private fb: FormBuilder) { }
