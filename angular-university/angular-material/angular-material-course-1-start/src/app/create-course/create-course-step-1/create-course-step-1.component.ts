736d1b0b9c0436ae6307c71edf5017a657786449
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-06-02 19:56:15 +0530

    added datepicker

--- angular-university/angular-material/angular-material-course-1-start/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ angular-university/angular-material/angular-material-course-1-start/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
+import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 const SAMPLE_TEXT="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium harum accusamus commodi mollitia earum architecto voluptatum enim, omnis quidem eligendi.";
 @Component({
   selector: "create-course-step-1",
@@ -19,7 +20,13 @@ export class CreateCourseStep1Component {
     downloadsAllowed: [false, Validators.requiredTrue],
     longDescription: [SAMPLE_TEXT, [Validators.required, Validators.minLength(3)]]
   });
-
+  dateClass:MatCalendarCellClassFunction<Date>=(cellDate,view)=>{
+      const date=cellDate.getDate();
+      if(view==="month"){
+        return (date==1)?'highlight-date':'';
+      }
+      return "";
+  }
   constructor(private fb: FormBuilder) { }
   get courseTitle() {
     return this.form.controls['title'];
