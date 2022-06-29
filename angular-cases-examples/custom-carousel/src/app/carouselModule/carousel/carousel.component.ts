b261402006d4a128bebd9b0f42d0cdee782ab932
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-06-29 19:25:27 +0530

    added slider

--- angular-cases-examples/custom-carousel/src/app/carouselModule/carousel/carousel.component.ts
+++ angular-cases-examples/custom-carousel/src/app/carouselModule/carousel/carousel.component.ts
@@ -10,10 +10,42 @@ interface carouselImage{
 })
 export class CarouselComponent implements OnInit {
   @Input() images:carouselImage[]=[];
+  @Input() indicators=true;
+  @Input() controls=true;
+  @Input() autoslide=false;
+  @Input() slideInterval=3000; //Default to 3 seconds
+
   selectedIndex=0;
   constructor() { }
 
   ngOnInit(): void {
+    if(this.autoslide){
+      this.autoSlideImages();
+    }
   }
-
+  // changes slide every <<slideInterval>> seconds
+  autoSlideImages():void{
+    setInterval(()=>{
+        this.onNextClick();
+    },this.slideInterval)
+  }
+  // sets index of image on dot/indicator click
+  selectImage(index:number):void{
+    this.selectedIndex=index;
+  }
+  // button click events
+  onPrevClick():void{
+  if(this.selectedIndex===0){
+    this.selectedIndex=this.images.length-1;
+  }else{
+    this.selectedIndex--;
+  }
+ }
+ onNextClick():void{
+  if(this.selectedIndex===this.images.length-1){
+    this.selectedIndex=0;
+  }else{
+    this.selectedIndex++;
+  }
+ }
 }
