fdeb0ac8b07b1ae872d2b5ee6cc32756870e8af3
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-06-29 13:56:15 +0530

    angular material passing data

--- angular-university/angular-material/angular-material-course-1-start/src/app/course-dialog/course-dialog.component.ts
+++ angular-university/angular-material/angular-material-course-1-start/src/app/course-dialog/course-dialog.component.ts
@@ -10,15 +10,43 @@ import * as moment from 'moment';
     styleUrls: ['./course-dialog.component.css']
 })
 export class CourseDialogComponent implements OnInit {
+    description:string;
 
-    constructor(private fb: FormBuilder) {
-
-
+    form=this.fb.group({
+        description:[this.course.description,Validators.required],
+        category:[this.course.category,Validators.required],
+        releasedAt:[new Date(),Validators.required],
+        longDescription:[this.course.longDescription,Validators.required],
+    })
+    //@Inject(MAT_DIALOG_DATA)-Inject the Dialog Input data passed via MatDialogConfig object
+    // dialogRef - passed to close the modal
+    constructor(private fb: FormBuilder,@Inject(MAT_DIALOG_DATA) private course:Course,private dialogRef:MatDialogRef<CourseDialogComponent>) {
+        this.description=course.description;
     }
-
     ngOnInit() {
 
     }
+    close(){
+        this.dialogRef.close();
+    }
+    save(){
+        this.dialogRef.close(this.form.value);
+    }
 
 }
+export function openEditCourseDialog(dialog:MatDialog,course:Course){
+ const config=new MatDialogConfig();
+ // escape click will not close popup
+ config.disableClose=true;
+ //focus on popup
+ config.autoFocus=true;
+ // pass data to dialog
+ config.data={
+    ...course
+ }
+//  open using dialog service
+const dialogRef=dialog.open(CourseDialogComponent,config);
+// return observable after dialog is closed
+return dialogRef.afterClosed();
 
+}
--- angular-university/angular-material/angular-material-course-1-start/src/app/courses-card-list/courses-card-list.component.ts
+++ angular-university/angular-material/angular-material-course-1-start/src/app/courses-card-list/courses-card-list.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {Course} from "../model/course";
-import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { MatDialog } from "@angular/material/dialog";
+import { openEditCourseDialog } from '../course-dialog/course-dialog.component';
+import { filter } from 'rxjs/operators';
 
 @Component({
     selector: 'courses-card-list',
@@ -11,7 +13,13 @@ export class CoursesCardListComponent implements OnInit {
     @Input()
     courses: Course[];
 
-    constructor() {   }
+    constructor(private dialog:MatDialog) {   }
     ngOnInit() {    }
-    editCourse(course:Course) {    }
+    // We have encapsulated the logic of opening the dialogue here in Save a Function, the open edit course dialog function.
+    editCourse(course:Course) {  
+        // open the modal
+        openEditCourseDialog(this.dialog,course).pipe(filter(val=>!!val)).subscribe(
+            val=>console.log("new course value:",val)
+        )
+    }
 }
