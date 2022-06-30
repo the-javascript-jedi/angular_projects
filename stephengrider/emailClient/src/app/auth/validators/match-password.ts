d16709f41d3481ea87b95be028ec9db41bf026ed
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-06-30 10:14:25 +0530

    added custom validator

---
+++ stephengrider/emailClient/src/app/auth/validators/match-password.ts
@@ -0,0 +1,15 @@
+import { AbstractControl, FormGroup, Validator } from "@angular/forms";
+import { Injectable } from "@angular/core";
+
+@Injectable({providedIn:'root'})
+export class MatchPassword implements Validator {
+    validate(formGroup:FormGroup) {
+        const {password,passwordConfirmation}=formGroup.value;
+        if(password===passwordConfirmation){
+            return null;
+        }else{
+            // return custom error
+            return {passwordsDontMatch:true}
+        }
+    }
+}
