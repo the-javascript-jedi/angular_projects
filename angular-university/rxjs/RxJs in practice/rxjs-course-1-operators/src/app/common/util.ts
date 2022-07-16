e6456e1529006c5e46db9efbb32e9e6d08ca808c
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-07-16 19:25:16 +0530

    changed file position

---
+++ angular-university/rxjs/RxJs in practice/rxjs-course-1-operators/src/app/common/util.ts
@@ -0,0 +1,13 @@
+import {Observable} from 'rxjs';
+export function createHttpObservable(url:string) {
+    return Observable.create(observer => {
+          fetch(url).then(response=>{
+                return response.json();
+            }).then(body=>{
+                observer.next(body);
+                observer.complete();
+            }).catch(err=>{
+                observer.console.error(err);                
+            })
+    })
+}
