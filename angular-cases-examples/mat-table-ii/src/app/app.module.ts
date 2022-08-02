e23661e18fa8cb3662c74f3c2f297376d9b6c69c
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-08-02 18:24:36 +0530

    added server and initial commit

--- angular-cases-examples/mat-table-ii/src/app/app.module.ts
+++ angular-cases-examples/mat-table-ii/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/material-module';
 
 @NgModule({
   declarations: [
