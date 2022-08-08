cf23ce4c4ef0dd5fc0d04727d0b1a3c9157afdd3
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-08-08 21:39:28 +0530

    added checkbox select all on table

--- angular-cases-examples/checkbox-table/src/app/app.component.ts
+++ angular-cases-examples/checkbox-table/src/app/app.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'checkbox-table';
+  masterSelected:boolean;
+  checklist:any;
+  checkedList:any;
+
+ constructor(){
+  this.masterSelected = false;
+   this.checklist =[
+    {id:1,isSelected:false,value:'dumpling'},
+    {id:2,isSelected:true,value:'burger'},
+    {id:3,isSelected:false,value:'sandwich'},
+  ]
+  this.getCheckedItemList();
+ }
+
+ // The master checkbox will check/ uncheck all items
+  checkUncheckAll() {
+    for (var i = 0; i < this.checklist.length; i++) {
+      this.checklist[i].isSelected = this.masterSelected;
+    }
+    this.getCheckedItemList();
+  }
+
+  // Check All Checkbox Checked
+  isAllSelected() {
+    this.masterSelected = this.checklist.every(function(item:any) {
+        return item.isSelected == true;
+      })
+    this.getCheckedItemList();
+  }
+
+  // Get List of Checked Items
+  getCheckedItemList(){
+    this.checkedList = [];
+    for (var i = 0; i < this.checklist.length; i++) {
+      if(this.checklist[i].isSelected)
+      this.checkedList.push(this.checklist[i]);
+    }
+    this.checkedList = JSON.stringify(this.checkedList);
+    console.log("this.checkedList ",this.checkedList )
+  }
+
+
 }
