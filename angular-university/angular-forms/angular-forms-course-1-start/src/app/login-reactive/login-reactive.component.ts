d974134201ce38802ad3cbce49f971e293805646
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-08-24 09:35:26 +0530

    added login form

--- angular-university/angular-forms/angular-forms-course-1-start/src/app/login-reactive/login-reactive.component.ts
+++ angular-university/angular-forms/angular-forms-course-1-start/src/app/login-reactive/login-reactive.component.ts
@@ -1,26 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { createPasswordStrengthValidator } from '../validators/password-strength.validator';
 @Component({
   selector: 'login',
   templateUrl: './login-reactive.component.html',
   styleUrls: ['./login-reactive.component.css']
 })
 export class LoginReactiveComponent implements OnInit {
-
-  form=new FormGroup({
-    email:new FormControl('',{
-      validators:[
-        Validators.required,
-        Validators.email
-      ]
-    }),
-    password:new FormControl('',{
-      validators:[
-        Validators.required,
-        Validators.minLength(3)
-      ]
-    })
+  //we can also specify the values outside the formGroup
+  //FormBuilder to define a form 
+  constructor(private fb:FormBuilder) {  }
+  form=this.fb.group({
+    // pass the validators as an array in second argument
+    // the third argument(if available) will contain an array of asynchronous validators
+    email:['',{
+      validators:[Validators.required,Validators.email],
+      updateOn:'blur'
+    }],
+    password:['',[Validators.required,
+        Validators.minLength(3),
+        // add a custom validator
+        createPasswordStrengthValidator()]]
   })
-  constructor() {  }
   ngOnInit() {  }
+  // getter for reactive forms
+  get email(){
+    return this.form.controls['email'];
+  }
+
+  get password(){
+    return this.form.controls['password'];
+  }
 }
