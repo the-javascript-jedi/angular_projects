24103b5f49b378f9bb0df3fb9dbcf7266fb655fb
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-08-24 09:50:16 +0530

    added non nullable form field

--- angular-university/angular-forms/angular-forms-course-1-start/src/app/login-reactive/login-reactive.component.ts
+++ angular-university/angular-forms/angular-forms-course-1-start/src/app/login-reactive/login-reactive.component.ts
@@ -10,13 +10,15 @@ export class LoginReactiveComponent implements OnInit {
   //we can also specify the values outside the formGroup
   //FormBuilder to define a form 
   constructor(private fb:FormBuilder) {  }
+  //do not specify the type so that we get automatic type safety
   form=this.fb.group({
     // pass the validators as an array in second argument
     // the third argument(if available) will contain an array of asynchronous validators
-    email:['',{
+    // we specify email the value as non nullable
+    email:this.fb.nonNullable.control("",{
       validators:[Validators.required,Validators.email],
       updateOn:'blur'
-    }],
+    }),
     password:['',[Validators.required,
         Validators.minLength(3),
         // add a custom validator
@@ -28,7 +30,22 @@ export class LoginReactiveComponent implements OnInit {
     return this.form.controls['email'];
   }
 
-  get password(){
+  get password(){    
     return this.form.controls['password'];
+  }
+
+  login(){
+    // we get type of value here
+    const formValue=this.form.value;
+    console.log("formValue.email",formValue.email);
+    console.log("formValue.password",formValue.password);
+  }
+
+  reset(){
+    // resets all values as null
+    //if value should not be null use this.fb.nonNullable.control for specific control
+    // to make entire form as non nullable specify the formbuilder as NonNullableFormBuilder
+    this.form.reset();
+    console.log("this.form.value",this.form.value);
   }
 }
