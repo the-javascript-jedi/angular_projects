0639c38cf7ad70f0e19fa0fe4090df82258da70a
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-08-25 22:23:11 +0530

    dynamic form control

--- angular-university/angular-forms/angular-forms-course-1-start/src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ angular-university/angular-forms/angular-forms-course-1-start/src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -10,9 +10,29 @@ import { CoursesService } from '../../services/courses.service';
 
 export class CreateCourseStep2Component implements OnInit {
   form=this.fb.group({
-    courseType:['premium',Validators.required]
+    courseType:['premium',Validators.required],
+    price:[null,[
+        Validators.required,
+        Validators.min(1),
+        Validators.max(9999),
+        Validators.pattern("[0-9]+")
+    ]]
   })
-  ngOnInit() {  }
+  ngOnInit() { 
+    // whenever the form changes any value this observable is updated
+    this.form.valueChanges.subscribe(val=>{
+      const priceControl=this.form.controls["price"];
+      // disable the price contol form field
+      if(val.courseType=='free'&&priceControl.enabled){
+        // if emitEvent is not kept as false there is a possibility of infinite loop
+        priceControl.disable({emitEvent:false});
+      }
+      else if(val.courseType=='premium'&&priceControl.disabled){
+        // if emitEvent is not kept as false there is a possibility of infinite loop
+        priceControl.enable({emitEvent:false});
+      }
+    })
+   }
 
   constructor(private fb:FormBuilder,private courses:CoursesService){  }
 }
