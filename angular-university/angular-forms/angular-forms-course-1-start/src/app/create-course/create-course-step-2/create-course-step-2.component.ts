9da293be101d2c8701bf9116be76c2856cf5a45b
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-08-25 21:31:36 +0530

    added step 2

--- angular-university/angular-forms/angular-forms-course-1-start/src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ angular-university/angular-forms/angular-forms-course-1-start/src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -1,19 +1,18 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-
+import { CoursesService } from '../../services/courses.service';
 
 @Component({
   selector: 'create-course-step-2',
   templateUrl: 'create-course-step-2.component.html',
   styleUrls: ['create-course-step-2.component.scss']
 })
+
 export class CreateCourseStep2Component implements OnInit {
+  form=this.fb.group({
+    courseType:['premium',Validators.required]
+  })
+  ngOnInit() {  }
 
-
-  ngOnInit() {
-
-
-
-  }
-
+  constructor(private fb:FormBuilder,private courses:CoursesService){  }
 }
--- angular-university/angular-forms/angular-forms-course-1-start/src/app/create-course/create-course.component.ts
+++ angular-university/angular-forms/angular-forms-course-1-start/src/app/create-course/create-course.component.ts
@@ -1,19 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
-
-
-
-
 @Component({
   selector: 'create-course',
   templateUrl: './create-course.component.html',
-  styleUrls: ['./create-course.component.scss']
+  styleUrls: ['./create-course.component.scss'],
+  // to show error message in stepper component
+  providers:[
+    {
+      provide:STEPPER_GLOBAL_OPTIONS,useValue:{showError:true}
+    }
+  ]
 })
 export class CreateCourseComponent implements OnInit {
-
-
-  ngOnInit() {
-
-  }
-
+  ngOnInit() { }
 }
