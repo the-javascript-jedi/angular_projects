a6db32298ceb04c87a49f0e027e0466903d3aa24
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-09-09 14:45:44 +0530

    added virtual scroll

--- angular-university/angular-material/angular-material-course-1-start/src/app/virtual-scrolling/virtual-scrolling.component.ts
+++ angular-university/angular-material/angular-material-course-1-start/src/app/virtual-scrolling/virtual-scrolling.component.ts
@@ -1,18 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-
-
 @Component({
   selector: "virtual-scrolling",
   templateUrl: 'virtual-scrolling.component.html',
   styleUrls: ["virtual-scrolling.component.scss"]
 })
 export class VirtualScrollingComponent implements OnInit {
-
-  items = Array.from({length: 100}).map((value, i) => `Item #${i}`);
-
+  items = Array.from({length: 200000}).map((value, i) => `Item #${i}`);
   ngOnInit() {
-
   }
-
-
 }
