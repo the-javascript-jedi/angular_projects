a370184381ce69ac141e8d15fb0e0449419e188c
unknown <nithin.thewebdev@gmail.com>
2022-11-25 09:50:53 +0530

    initial commit angular auth

---
+++ codevolution/AngularAuth/server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const PORT = 5000;
+const api = require("./routes/api");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/api", api);
+app.get("/", function (req, res) {
+  res.send("Hello from Server");
+});
+
+app.listen(PORT, function () {
+  console.log("Server running on localhost:" + PORT);
+});
