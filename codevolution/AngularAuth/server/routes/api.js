5b0599b9ac335877fa100ca57df459e68cd42520
unknown <nithin.thewebdev@gmail.com>
2022-11-26 11:22:25 +0530

    added connectio string

--- codevolution/AngularAuth/server/routes/api.js
+++ codevolution/AngularAuth/server/routes/api.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+const mongoose = require("mongoose");
+
+const db =
+  "mongodb+srv://nithin123:nithin123@cluster0.vm99a.mongodb.net/eventsdb";
+
+mongoose.connect(db, (err) => {
+  if (err) {
+    console.error("Error!" + err);
+  } else {
+    console.error("Connected to mongodb");
+  }
+});
 router.get("/", (req, res) => {
   res.send("From API route");
 });
