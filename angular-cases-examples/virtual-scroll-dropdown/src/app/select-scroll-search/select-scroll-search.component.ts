d1bec048cdec2fa1f2b918b71aa3709c67acc40b
the-javascript-jedi <nithin.thewebdev@gmail.com>
2022-12-15 21:36:45 +0530

    updated scroll with search

--- angular-cases-examples/virtual-scroll-dropdown/src/app/select-scroll-search/select-scroll-search.component.ts
+++ angular-cases-examples/virtual-scroll-dropdown/src/app/select-scroll-search/select-scroll-search.component.ts
@@ -22,8 +22,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SelectScrollSearchComponent implements OnInit,AfterViewInit {
   @ViewChild('searchInput', { static: true }) searchInput: ElementRef;
-  @ViewChild('indication', { static: true }) indication: ElementRef;
+  @ViewChild('searchBox', { static: true }) searchBox: ElementRef;
+  @ViewChild('scrollContainer', { static: true }) scrollContainer: ElementRef;
   @Input() apiEndpoint = '';
+  @Input() defaultDropdownValue = '';
   @Output() selectedItemEvent = new EventEmitter<string>();
 
   responseData=[];
@@ -36,6 +38,9 @@ export class SelectScrollSearchComponent implements OnInit,AfterViewInit {
     this._renderer.listen('window', 'click',(e:Event)=>{
       if( e.target!==this.searchInput.nativeElement){
             this.displayDropdown=false;
+            if(this.dropdownSelectedValue==''&&this.dropdownValueFocus!==''){
+              this.dropdownSelectedValue=this.dropdownValueFocus;
+            }
         }
     })
   }
@@ -43,35 +48,37 @@ export class SelectScrollSearchComponent implements OnInit,AfterViewInit {
   ngOnInit(): void {
     this.loadResponse().subscribe({
       next:(res)=>{
+        console.log("1 ngOninit called");
         this.responseData=res;
       }
     })
+    this.dropdownValueFocus=this.defaultDropdownValue;
   }
 
   ngAfterViewInit() {
     //distinctUntilChanged() - If two consecutive values are exactly the same, we only want to emit one value and we can get that functionality using the distinct until changed operator with this operator, we no longer will have duplicate values in our output.
         //switchMap()-switch map is going to unsubscribe from the search that was still ongoing when we type new value.That is an outdated search and the Http  request is going to be canceled. 
-        const searchLessons$=fromEvent<any>(this.searchInput.nativeElement,'keyup').pipe(
+        const searchDropdownData$=fromEvent<any>(this.searchInput.nativeElement,'keyup').pipe(
             map(event=>event.target.value),
             debounceTime(400),
             distinctUntilChanged(),
             switchMap(search=>this.loadResponse(search))
         )
         
-        searchLessons$.subscribe({
+        searchDropdownData$.subscribe({
       next:(res)=>{
         this.responseData=res;
       }
     })
     // scroll
-     this.indication.nativeElement.addEventListener('scroll', () => {
+     this.scrollContainer.nativeElement.addEventListener('scroll', () => {
     //  let listElement: HTMLElement
         console.log("scrolled"); 
         // console.log("listElement.scrollTop",this.indication.nativeElement.scrollTop); 
         // console.log("listElement.offsetHeight",this.indication.nativeElement.offsetHeight); 
-        console.log("listElement.scrollTop + listElement.offsetHeight",this.indication.nativeElement.scrollTop + this.indication.nativeElement.offsetHeight); 
-        console.log("listElement.scrollHeight",this.indication.nativeElement.scrollHeight); 
-         if (((this.indication.nativeElement.scrollTop + this.indication.nativeElement.offsetHeight)) >= (this.indication.nativeElement.scrollHeight+17)) {
+        console.log("listElement.scrollTop + listElement.offsetHeight",this.scrollContainer.nativeElement.scrollTop + this.scrollContainer.nativeElement.offsetHeight); 
+        console.log("listElement.scrollHeight",this.scrollContainer.nativeElement.scrollHeight); 
+         if (((this.scrollContainer.nativeElement.scrollTop + this.scrollContainer.nativeElement.offsetHeight)) >= (this.scrollContainer.nativeElement.scrollHeight+17)) {
           console.log("listElement.make api call");
           // const searchGamesOnScroll$=fromEvent<any>(this.loadGames('',1).subscribe());
           console.log("this.responseData.length",this.responseData.length);
@@ -93,9 +100,12 @@ export class SelectScrollSearchComponent implements OnInit,AfterViewInit {
 
   clearSearchText(){
     this.dropdownSelectedValue="";
-    this.indication.nativeElement.scrollTop = 0;
+    this.dropdownValueFocus='';
+    this.dropdownValueFocus=this.defaultDropdownValue;
+    this.scrollContainer.nativeElement.scrollTop = 0;
      this.loadResponse().subscribe({
       next:(res)=>{
+        console.log("2 clearSearchText called");
         this.responseData=res;
       }
     })
@@ -103,12 +113,22 @@ export class SelectScrollSearchComponent implements OnInit,AfterViewInit {
   inputSearchKeyUp(event){
     console.log("event",event)
     if(event.target.value!=''){
-        this.indication.nativeElement.scrollTop = 0;
+        this.scrollContainer.nativeElement.scrollTop = 0;
       }
   }
   focusSearchKeyUp(){
-    this.indication.nativeElement.scrollTop = 0;
+    this.scrollContainer.nativeElement.scrollTop = 0;
     this.displayDropdown=true;  
+    if(this.dropdownSelectedValue!=''){
+      this.dropdownValueFocus=this.dropdownSelectedValue;
+    }
+    this.dropdownSelectedValue='';
+    this.loadResponse().subscribe({
+      next:(res)=>{
+        console.log("3 focusSearchKeyUp called");
+        this.responseData=res;
+      }
+    })
   }
   selectDropdownItem(value: string) {
     this.selectedItemEvent.emit(value);
@@ -121,8 +141,13 @@ export class SelectScrollSearchComponent implements OnInit,AfterViewInit {
   }
   selectListItem(selectedItem){
     console.log("selectedItem",selectedItem);
-    this.dropdownSelectedValue=selectedItem.description;
-    this.selectDropdownItem(selectedItem.description);
+    if(selectedItem===this.defaultDropdownValue){
+      this.dropdownSelectedValue=this.defaultDropdownValue;
+      this.selectDropdownItem(this.defaultDropdownValue);
+    }else{
+      this.dropdownSelectedValue=selectedItem.description;
+      this.selectDropdownItem(selectedItem.description);
+    }
     this.displayDropdown=false;
   }
 }
