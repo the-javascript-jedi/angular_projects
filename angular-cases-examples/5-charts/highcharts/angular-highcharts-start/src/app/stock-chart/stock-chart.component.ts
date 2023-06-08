516ab07410b95e3cfaccf956efce50444c5c9a95
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-08 14:40:01 +0530

    added real time data

--- angular-cases-examples/5-charts/highcharts/angular-highcharts-start/src/app/stock-chart/stock-chart.component.ts
+++ angular-cases-examples/5-charts/highcharts/angular-highcharts-start/src/app/stock-chart/stock-chart.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit} from "@angular/core";
+import * as moment from 'moment';
 import * as Highcharts from "highcharts/highstock";
 import { Options } from "highcharts/highstock";
+import {dataFromApiTS} from "./data/dummyData"
 
 import IndicatorsCore from "highcharts/indicators/indicators";
 import IndicatorZigzag from "highcharts/indicators/zigzag";
@@ -13,12 +15,48 @@ IndicatorZigzag(Highcharts);
   styleUrls: ['./stock-chart.component.scss']
 })
 export class StockChartComponent implements OnInit {
-  Highcharts: typeof Highcharts = Highcharts;
+  Highcharts: any = Highcharts;
 
   constructor() { }
+  dataFromAPIS:any;
+  chartData:any=[]
 
   ngOnInit(): void {
      console.log("window",window);
+       // load data from apis
+    this.dataFromAPIS=dataFromApiTS.data;
+    console.log("this.dataFromAPIS",this.dataFromAPIS);
+    let chartDataFormat=this.dataFromAPIS.map(data=>{
+      let dumpDateFromAPI=data.dumpDate;
+      let yearDumpDate=dumpDateFromAPI.substring(0,4);
+      let monthDumpDate=dumpDateFromAPI.substring(4,6);
+      let dayDumpDate=dumpDateFromAPI.substring(6,8);
+      let dumpDateWithSlash=yearDumpDate+'-'+monthDumpDate+'-'+dayDumpDate;
+      const convertedDumpDate=moment(dumpDateWithSlash,'YYYY-MM-DD').toDate();
+      data.dumpDateTimeStamp=convertedDumpDate;
+      data.dateInMilliSeconds=convertedDumpDate.getTime();
+      // const convertedDumpDate=moment
+      return data;
+    });
+// chartDataFormat.sort(function (a, b) {
+//       // var dateA = new Date(a.dateInMilliSeconds);
+//       // var dateB = new Date(b.dateInMilliSeconds);
+//       return a.dateInMilliSeconds - b.dateInMilliSeconds;
+//     });
+// sort the chart data
+    chartDataFormat.sort(function compare(a, b) {
+      var dateA = new Date(a.dateInMilliSeconds).valueOf();
+      var dateB = new Date(b.dateInMilliSeconds).valueOf();
+      return dateA - dateB;
+    });
+    // create an array of arrays for the highcharts stock chart
+    chartDataFormat.forEach(val=>{
+      let chartArray=[];
+      chartArray.push(val.dateInMilliSeconds);
+      chartArray.push(val.ciscoMlFinal);
+      this.chartData.push(chartArray)
+    })
+    console.log("this.chartData",this.chartData);
   }
 
     chartOptions: Options = {
@@ -27,41 +65,21 @@ export class StockChartComponent implements OnInit {
         type: "line",
         id: "base",
         pointInterval: 24 * 3600 * 1000,
-        data: [
-          [1528205400000, 100],
-          [1528291800000, 101],
-          [1528378200000, 200],
-          [1528464600000, 150],
-          [1528723800000, 190],
-          [1528810200000, 102],
-          [1528896600000, 103],
-          [1528983000000, 104],
-          [1529069400000, 105],
-          [1529328600000, 106],
-          [1529415000000,107],
-          [1529501400000, 51],
-          [1529587800000, 15],
-          [1529674200000, 52],
-          [1529933400000, 53],
-          [1530019800000, 54],
-          [1530106200000, 55],
-          [1530192600000, 56],
-          [1530279000000, 71],
-          [1530538200000, 72],
-          [1530624600000, 187.79],
-          [1530797400000, 185.26],
-          [1530883800000, 185.42],
-          [1531143000000, 189.5],
-          [1531229400000, 190.71],
-          [1531315800000, 188.5],
-          [1531402200000, 189.53],
-          [1531488600000, 191.08],
-          [1531747800000, 191.52],
-          [1531834200000, 189.75],
-          [1531920600000, 191.78],
-          [1532007000000, 189.69],        
-        ]
-      }     
+        data: this.chartData,
+      },     
+      {
+        type: "zigzag",
+        showInLegend: true,
+        linkedTo: "base"
+      }
     ]
   };
+
+  getDates(){
+    // var extremes = this.Highcharts.Series
+   var extremes = this.chartOptions.xAxis
+    console.log("extremes",extremes)
+    console.log("this.chartOptions",this.chartOptions)
+    console.log("this.Highcharts",this.Highcharts.Axis.x)
+  }
 }
