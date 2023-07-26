d07601d42a084f113d7ecd66eb88727d767df10b
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-07-26 19:36:06 +0530

    added routes

--- angular-university/angular-router/angular-router-course-1-start/src/app/app-routing.module.ts
+++ angular-university/angular-router/angular-router-course-1-start/src/app/app-routing.module.ts
@@ -1,14 +1,23 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
 
-
+// routing config corresponds to a particular screen
 const routes: Routes = [
-
+  {
+    path:"login",
+    component:LoginComponent
+  },
+  {
+    path:"about",
+    component:AboutComponent
+  },
 ];
 
 @NgModule({
   imports: [
-
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
   providers: [
@@ -16,6 +25,5 @@ const routes: Routes = [
   ]
 })
 export class AppRoutingModule {
-
 
 }
