0ea127d19f8699b8c4847382fb335e5f278388b3
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-08-02 09:22:06 +0530

    added response interceptor

--- angular-cases-examples/1-table-projects/table-horizontal-bar-graph/src/app/services/auth-interceptor.service.ts
+++ angular-cases-examples/1-table-projects/table-horizontal-bar-graph/src/app/services/auth-interceptor.service.ts
@@ -1,14 +1,26 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { tap } from "rxjs";
 
 export class AuthInterceptorService implements HttpInterceptor{
     intercept(req:HttpRequest<any>,next:HttpHandler){
         // we can handle the url here using below code
         console.log("req.url",req.url)
-        // if(req.url==""){
-            
-        // }
-        // after modifying the request we must return request.handle
+        // if(req.url==""){}
         console.log("Sending Request Interceptor");
-        return next.handle(req);
+        // modify the request
+        let modifiedRequest=req.clone({
+            // headers:req.headers.append('test','abc'),
+            // params:req.params.append('hi','hello world')
+        })
+
+        // after modifying the request we must return request.handle
+        // we handle the response by adding pipe as it is an observable
+        return next.handle(modifiedRequest).pipe(tap(event=>{
+            console.log("event",event);
+            console.log("Response from interceptor");
+            if(event.type===HttpEventType.Response){
+                console.log("event.body",event.body);
+            }
+        }));
     }
 }
