1308de4ab14a12c916d275939cce9fd16138ea8a
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-01-09 17:16:18 +0530

    added  cart page service

--- angular_projects/online-food-store/frontend/src/app/food-page/food-page.component.ts
+++ angular_projects/online-food-store/frontend/src/app/food-page/food-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-// import { CartService } from '../services/cart/cart.service';
+import { CartService } from '../services/cart/cart.service';
 import { FoodService } from '../services/food/food.service';
 import { Food } from '../shared/models/Food';
 
@@ -14,7 +14,7 @@ export class FoodPageComponent implements OnInit {
   food!: Food;
   constructor(private activatedRoute:ActivatedRoute,
     private foodService: FoodService,
-    // private cartService: CartService,
+    private cartService: CartService,
     private router: Router) { 
     activatedRoute.params.subscribe((params) => {
       if(params.id)
@@ -27,7 +27,7 @@ export class FoodPageComponent implements OnInit {
   }
 
   addToCart(){
-    // this.cartService.addToCart(this.food);
+    this.cartService.addToCart(this.food);
     this.router.navigateByUrl('/cart-page');
   }
 onClickResult: any;
