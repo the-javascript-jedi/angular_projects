971e8b5a51a247de8b603b28b59cd482601b15c4
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-03-18 23:32:27 +0530

    added page not found

--- angular-university/angular-router/angular-router-course-1-start/src/app/app-routing.module.ts
+++ angular-university/angular-router/angular-router-course-1-start/src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 // routing config corresponds to a particular screen
 const routes: Routes = [
-   {
+  // root path - redirect to the courses path 
+  // empty path will match everything so we need to add pathMatch:"full" to match path exactly
+  {
+    path:"",
+    redirectTo:"/courses",
+    pathMatch:"full"
+  },
+  {
     path:"courses",
     // all routes with courses/ are children of courses route
     loadChildren:()=>import('./courses/courses.module').then(m=>m.CoursesModule)
@@ -18,6 +26,12 @@ const routes: Routes = [
     path:"about",
     component:AboutComponent
   },
+  {
+    // ** - wildcard matches anything we type in the address bar
+    // always wildcard put at end of routing module paths
+    path:"**",
+    component:PageNotFoundComponent
+  }
 ];
 
 @NgModule({
