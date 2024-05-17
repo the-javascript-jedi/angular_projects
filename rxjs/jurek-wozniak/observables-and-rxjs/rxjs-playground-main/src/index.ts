a6665335621e80255c5b81fa938b0cadb1f9823a
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-05-17 09:15:19 +0530

    added observer unsubscribe

---
+++ rxjs/jurek-wozniak/observables-and-rxjs/rxjs-playground-main/src/index.ts
@@ -0,0 +1,22 @@
+import { Observable } from "rxjs";
+
+
+const interval$ = new Observable<number>(subscriber => {
+  let counter = 1;
+
+  const intervalId = setInterval(() => {
+    console.log('Emitted', counter);
+    subscriber.next(counter++);
+  }, 2000);
+  //observable cleanup -without this observer will keep on emitting even after unsubscribing
+  return () => {
+    clearInterval(intervalId);
+  };
+});
+
+const subscription = interval$.subscribe(value => console.log(value));
+
+setTimeout(() => {
+  console.log('Unsubscribe');
+  subscription.unsubscribe();
+}, 7000);
