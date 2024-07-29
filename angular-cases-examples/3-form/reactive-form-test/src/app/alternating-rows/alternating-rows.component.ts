7a3e82a1d83adf57f674ccc579472e748c04fcb6
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-07-29 13:44:42 +0530

    added alternating row

--- angular-cases-examples/3-form/reactive-form-test/src/app/alternating-rows/alternating-rows.component.ts
+++ angular-cases-examples/3-form/reactive-form-test/src/app/alternating-rows/alternating-rows.component.ts
@@ -5,57 +5,51 @@ import {
     ViewChild,
     HostListener,
     AfterViewInit
-}
-
-from '@angular/core';
+} from '@angular/core';
 
 @Component({
     selector: 'app-alternating-rows',
     templateUrl: './alternating-rows.component.html',
     styleUrls: ['./alternating-rows.component.scss']
+})
+export class AlternatingRowsComponent implements OnInit, AfterViewInit {
+    @ViewChild('container') container!: ElementRef;
 
-}) export class AlternatingRowsComponent implements OnInit,
-AfterViewInit {
-    @ViewChild('container') container !: ElementRef;
+    boxes = Array.from({ length: 20 }, (_, i) => i + 1);
+    rows: number[][] = [];
 
-    boxes=Array.from({
-        length: 20
+    ngOnInit(): void {}
+
+    ngAfterViewInit(): void {
+        this.arrangeBoxes();
     }
 
-    , (_, i)=> i + 1);
-rows: number[][]=[];
+    @HostListener('window:resize', ['$event'])
+    onResize(event: any) {
+        this.arrangeBoxes();
+    }
 
-ngOnInit(): void {}
+    arrangeBoxes(): void {
+        this.rows = [];
+        const containerWidth = this.container.nativeElement.offsetWidth;
+        let currentRow: number[] = [];
+        let rowWidth = 0;
 
-ngAfterViewInit(): void {
-    this.arrangeBoxes();
-}
-
-@HostListener('window:resize', ['$event']) onResize(event: any) {
-    this.arrangeBoxes();
-}
-
-arrangeBoxes(): void {
-    this.rows=[];
-    const containerWidth=this.container.nativeElement.offsetWidth;
-    let currentRow: number[]=[];
-    let rowWidth=0;
-
-    this.boxes.forEach(number=> {
-            const boxWidth=500; // Box width + margin
-            rowWidth +=boxWidth;
+        this.boxes.forEach(number => {
+            const boxWidth = 300 + 10; // Box width + margin
+            rowWidth += boxWidth;
 
             if (rowWidth > containerWidth) {
                 this.rows.push(currentRow);
-                currentRow=[];
-                rowWidth=boxWidth;
+                currentRow = [];
+                rowWidth = boxWidth;
             }
 
             currentRow.push(number);
         });
 
-    if (currentRow.length > 0) {
-        this.rows.push(currentRow);
+        if (currentRow.length > 0) {
+            this.rows.push(currentRow);
+        }
     }
-}
 }
