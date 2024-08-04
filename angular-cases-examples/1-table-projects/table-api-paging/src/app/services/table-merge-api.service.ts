94f26490d514f337ef7ad5681891fcb48e57781a
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-08-04 20:25:33 +0530

    added comments

--- angular-cases-examples/1-table-projects/table-api-paging/src/app/services/table-merge-api.service.ts
+++ angular-cases-examples/1-table-projects/table-api-paging/src/app/services/table-merge-api.service.ts
@@ -3,15 +3,11 @@ import {HttpClient} from "@angular/common/http"
 
 @Injectable() 
 export class TableMergeApiService {
-
   constructor(private _http:HttpClient) { }
-
   loadUsers(id){
     return this._http.get(`https://jsonplaceholder.typicode.com/users/${id}`)
   }
-
    loadPosts(id){
     return this._http.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
-  }
-  
+  }  
 }
