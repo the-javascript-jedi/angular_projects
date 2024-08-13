51e4a5b5edef495f9f8db58831c8e1a271f99b3e
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-08-13 09:50:49 +0530

    service worker demo

---
+++ angular-cases-examples/angular-service-worker/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { FormTestComponent } from './form-test/form-test.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormTestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
