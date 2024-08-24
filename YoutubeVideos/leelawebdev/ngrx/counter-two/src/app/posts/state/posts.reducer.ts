2ec409fbc0525f0c5226cb9595f8277cecf9b69f
the-javascript-jedi <nithin.thewebdev@gmail.com>
2024-08-24 17:23:50 +0530

    insert post

--- YoutubeVideos/leelawebdev/ngrx/counter-two/src/app/posts/state/posts.reducer.ts
+++ YoutubeVideos/leelawebdev/ngrx/counter-two/src/app/posts/state/posts.reducer.ts
@@ -1,7 +1,16 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
 import { initialState } from "./posts.state";
+import { addPost } from "./posts.action";
 
-const _postsReducer=createReducer(initialState)
+const _postsReducer=createReducer(initialState,on(addPost,(state,action)=>{
+    let post={...action.post};
+    // add a post
+    post.id=(state.posts.length+1).toString();
+    return {
+        ...state,
+        posts:[...state.posts,post]
+    }
+}))
 
 export function postsReducer(state,action){
     return _postsReducer(state,action);
