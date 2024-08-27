a234cf5be1d7bf406019e0f58266d6d62d720781
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-08-27 18:46:24 +0530

    added ngrx

--- OleksandrKocherhin/ngrx/mediumclone_angular/src/app/auth/components/register/register.component.ts
+++ OleksandrKocherhin/ngrx/mediumclone_angular/src/app/auth/components/register/register.component.ts
@@ -1,11 +1,18 @@
 import {Component} from '@angular/core'
 import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms'
+import { Store } from '@ngrx/store'
+import { register } from '../../store/actions';
+import { RegisterRequestInterface } from '../../types/registerRequest.interface';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { selectIsSubmitting } from '../../store/selectors';
+import { AuthStateInterface } from '../../types/authState.interface';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,RouterLink,CommonModule],
 })
 export class RegisterComponent {
   form = this.fb.nonNullable.group({
@@ -13,10 +20,17 @@ export class RegisterComponent {
     email: ['', Validators.required],
     password: ['', Validators.required],
   })
+  // get the changed state
+  isSubmitting$=this.store.select(selectIsSubmitting)
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder,private store:Store<{auth:AuthStateInterface}>) {}
 
   onSubmit() {
-    console.log('form', this.form.getRawValue())
+    console.log('form', this.form.getRawValue());
+    const request:RegisterRequestInterface={
+      user:this.form.getRawValue()
+    };
+    // dispatch an action
+    this.store.dispatch(register({request}))
   }
 }
