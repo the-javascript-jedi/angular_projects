001746feb2f8d8ee23991529f243d44caaa8f3e6
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-11-20 17:41:29 +0530

    added tpbar

--- OleksandrKocherhin/ngrx/mediumclone_angular/src/app/app.component.ts
+++ OleksandrKocherhin/ngrx/mediumclone_angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core'
 import {RouterOutlet} from '@angular/router'
+import { TopBarComponent } from './shared/components/top-bar/top-bar.component'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TopBarComponent],
 })
 export class AppComponent {}
---
+++ OleksandrKocherhin/ngrx/mediumclone_angular/src/app/shared/components/top-bar/top-bar.component.ts
@@ -0,0 +1,19 @@
+import {CommonModule} from '@angular/common'
+import {Component} from '@angular/core'
+import {RouterLink} from '@angular/router'
+import {Store} from '@ngrx/store'
+import {combineLatest} from 'rxjs'
+import {selectCurrentUser} from 'src/app/auth/store/reducers'
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  standalone: true,
+  imports: [RouterLink, CommonModule],
+})
+export class TopBarComponent {
+data$ = combineLatest({
+    currentUser: this.store.select(selectCurrentUser),
+  })
+  constructor(private store: Store) {}
+}
