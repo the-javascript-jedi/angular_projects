c9bf8cf41be7e35ab697f097cf14b6965e71bffe
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-11-27 23:22:39 +0530

    added map

--- rxjs/jurek-wozniak/observables-and-rxjs/rxjs-playground-main/src/index.ts
+++ rxjs/jurek-wozniak/observables-and-rxjs/rxjs-playground-main/src/index.ts
@@ -1,29 +1,26 @@
-import { Observable } from "rxjs";
-import { filter } from "rxjs/operators";
 
 
-interface NewsItem {
-  category: 'Business' | 'Sports';
-  content: string;
-}
+import { forkJoin } from "rxjs";
+// Mike is from New Delhi and likes to eat pasta.
 
-const newsFeed$ = new Observable<NewsItem>(subscriber => {
-  setTimeout(() => 
-    subscriber.next({ category: 'Business', content: 'A' }), 1000);
-  setTimeout(() => 
-    subscriber.next({ category: 'Sports', content: 'B' }), 3000);
-  setTimeout(() => 
-    subscriber.next({ category: 'Business', content: 'C' }), 4000);
-  setTimeout(() => 
-    subscriber.next({ category: 'Sports', content: 'D' }), 6000);
-  setTimeout(() => 
-    subscriber.next({ category: 'Business', content: 'E' }), 7000);
-});
+import { ajax } from "rxjs/ajax";
+import { map } from "rxjs/operators";
 
-const sportsNewsFeed$ = newsFeed$.pipe(
-  filter(item => item.category === 'Sports')
+const randomFirstName$ = ajax<any>('https://random-data-api.com/api/name/random_name').pipe(
+  map(ajaxResponse => ajaxResponse.response.first_name)
 );
 
-sportsNewsFeed$.subscribe(
-  item => console.log(item)
+const randomCapital$ = ajax<any>('https://random-data-api.com/api/nation/random_nation').pipe(
+  map(ajaxResponse => ajaxResponse.response.capital)
 );
+
+const randomDish$ = ajax<any>('https://random-data-api.com/api/food/random_food').pipe(
+  map(ajaxResponse => ajaxResponse.response.dish)
+);
+
+forkJoin([randomFirstName$, randomCapital$, randomDish$]).subscribe(
+  ([firstName, capital, dish]) =>
+    console.log(`${firstName} is from ${capital} and likes to eat ${dish}.`)
+);
+
+////////////////////////////////////////////////////////////////////////////
