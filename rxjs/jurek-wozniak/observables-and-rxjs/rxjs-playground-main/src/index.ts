7dd84ed30624d1e3893647a395a496c43a43d1ba
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-11-28 13:26:04 +0530

    added debounce

--- rxjs/jurek-wozniak/observables-and-rxjs/rxjs-playground-main/src/index.ts
+++ rxjs/jurek-wozniak/observables-and-rxjs/rxjs-playground-main/src/index.ts
@@ -1,26 +1,14 @@
+import { of } from "rxjs";
+import { filter, map, tap } from "rxjs/operators";
 
 
-import { forkJoin } from "rxjs";
-// Mike is from New Delhi and likes to eat pasta.
-
-import { ajax } from "rxjs/ajax";
-import { map } from "rxjs/operators";
-
-const randomFirstName$ = ajax<any>('https://random-data-api.com/api/name/random_name').pipe(
-  map(ajaxResponse => ajaxResponse.response.first_name)
-);
-
-const randomCapital$ = ajax<any>('https://random-data-api.com/api/nation/random_nation').pipe(
-  map(ajaxResponse => ajaxResponse.response.capital)
-);
-
-const randomDish$ = ajax<any>('https://random-data-api.com/api/food/random_food').pipe(
-  map(ajaxResponse => ajaxResponse.response.dish)
-);
-
-forkJoin([randomFirstName$, randomCapital$, randomDish$]).subscribe(
-  ([firstName, capital, dish]) =>
-    console.log(`${firstName} is from ${capital} and likes to eat ${dish}.`)
-);
-
-////////////////////////////////////////////////////////////////////////////
+of(1, 7, 3, 6, 2).pipe(
+  tap({
+    next: value => console.log('Spy 1:', value)
+  }),
+  filter(value => value > 5),
+  tap({
+    next: value => console.log('Spy 2:', value)
+  }),
+  map(value => value * 2),
+).subscribe(value => console.log('Output:', value));
