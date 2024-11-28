dd60579bd2d39089b5f267571f01dbe8a41f5336
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-11-28 22:21:09 +0530

    added concat map error handling

--- rxjs/jurek-wozniak/observables-and-rxjs/rxjs-playground-main/src/index.ts
+++ rxjs/jurek-wozniak/observables-and-rxjs/rxjs-playground-main/src/index.ts
@@ -1,20 +1,19 @@
-import { EMPTY, Observable, of } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { EMPTY, fromEvent, of } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { catchError, concatMap, map } from "rxjs/operators";
 
+const endpointInput: HTMLInputElement = document.querySelector('input#endpoint');
+const fetchButton = document.querySelector('button#fetch');
 
-const failingHttpRequest$ = new Observable(subscriber => {
-  setTimeout(() => {
-    subscriber.error(new Error('Timeout'));
-  }, 3000);
-});
-
-console.log('App started');
-
-failingHttpRequest$.pipe(
-  // catchError(error => EMPTY)
-  catchError(error=> of('Fallback value'))
+fromEvent(fetchButton, 'click').pipe(
+  map(() => endpointInput.value),
+  concatMap(value =>
+    ajax(`https://random-data-api.com/api/${value}/random_${value}`).pipe(
+      // catchError(error => of(`Could not fetch data: ${error}`))
+    )
+  )
 ).subscribe({
   next: value => console.log(value),
-  error:errorValue=>{console.log("value of error",errorValue)},
+  error: err => console.log('Error:', err),
   complete: () => console.log('Completed')
 });
