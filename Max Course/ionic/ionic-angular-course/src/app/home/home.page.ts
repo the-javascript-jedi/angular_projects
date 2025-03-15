fd01816cc8f2e2283832ecbbc7267ed4151fb299
Nithin Samuel <nithin.thewebdev@gmail.com>
2025-03-15 23:32:13 +0530

    INitial commit

---
+++ Max Course/ionic/ionic-angular-course/src/app/home/home.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+
+  constructor() {}
+  text:any;
+  onChangeText(){
+    this.text="Hello World";
+  }
+}
