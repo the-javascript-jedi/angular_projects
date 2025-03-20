914f337c7793689b26d4efdcec7425fcb5b01caf
Nithin Samuel <nithin.thewebdev@gmail.com>
2025-03-20 23:29:48 +0530

    added angular app and renamed the js folder

---
+++ Max Course/ionic/ionic-app-course/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {
+  RouteReuseStrategy,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    BrowserModule,
+    IonicModule.forRoot(),
+    IonicModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
