287b990d64dbb480cffae19f4dd1de03af520b1f
Nithin Samuel <nithin.thewebdev@gmail.com>
2025-03-23 23:33:21 +0530

    added recipes page

--- Max Course/ionic/ionic-app-course/src/app/app-routing.module.ts
+++ Max Course/ionic/ionic-app-course/src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomePage } from './home/home.page';
+// import { HomePage } from './home/home.page';
 
 const routes: Routes = [
+  {path:'',redirectTo:'recipes',pathMatch:'full'},
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   component: HomePage,
+  // },
+
   {
-    path: '',
-    pathMatch: 'full',
-    component: HomePage,
+    path: 'recipes',
+    loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
   },
 ];
 
---
+++ Max Course/ionic/ionic-app-course/src/app/recipes/recipes.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.page.html',
+  styleUrls: ['./recipes.page.scss'],
+  standalone:false
+})
+export class RecipesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
