adffab454f2ec4eae592e860e87192e3f3af6a5a
Nithin Samuel <nithin.thewebdev@gmail.com>
2025-04-14 12:46:16 +0530

    added fixes for showing names

--- Max Course/ionic/ionic-courses-project/src/app/places/discover/place-detail/place-detail.page.ts
+++ Max Course/ionic/ionic-courses-project/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { PlacesService } from '../../places.service';
+import { Place } from 'src/app/models/place.model';
 
 @Component({
   selector: 'app-place-detail',
@@ -10,19 +12,29 @@ import { NavController } from '@ionic/angular';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor(private router: Router, private navCtrl: NavController, private route: ActivatedRoute) { }
-
+  constructor(private router: Router, private navCtrl: NavController, private route: ActivatedRoute, private placesService: PlacesService) { }
+  place: Place | null = null;
   ngOnInit() {
     this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/discover');
+        return;
+      }
 
       const placeId = paramMap.get('placeId')!;
-      console.log("placeId", placeId)
+      const fetchedPlace = this.placesService.getPlace(placeId);
+
+      if (!fetchedPlace) {
+        this.navCtrl.navigateBack('/places/tabs/discover');
+        return;
+      }
+
+      this.place = fetchedPlace;
     });
   }
 
   onBookplace() {
     console.log('Booking place...');
-    // this.router.navigate(['/places/tabs/discover']);
     this.navCtrl.navigateBack('/places/tabs/discover');
   }
 }
--- Max Course/ionic/ionic-courses-project/src/app/places/offers/edit-offer/edit-offer.page.ts
+++ Max Course/ionic/ionic-courses-project/src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { PlacesService } from '../../places.service';
+import { Place } from 'src/app/models/place.model';
 
 @Component({
   selector: 'app-edit-offer',
@@ -9,12 +12,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EditOfferPage implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private navCtrl: NavController, private placesService: PlacesService) { }
+  place: Place | null = null;
 
   ngOnInit() {
     this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return;
+      }
+
       const placeId = paramMap.get('placeId')!;
-      console.log("offer-placeId", placeId)
+      const fetchedPlace = this.placesService.getPlace(placeId);
+
+      if (!fetchedPlace) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return;
+      }
+
+      this.place = fetchedPlace;
     });
   }
 
