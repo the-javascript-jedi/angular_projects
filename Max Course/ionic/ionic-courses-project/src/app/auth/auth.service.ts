745b788ba528dbab30d978a2f822c86322083d6e
Nithin Samuel <nithin.thewebdev@gmail.com>
2025-04-17 07:46:42 +0530

    added auth service

---
+++ Max Course/ionic/ionic-courses-project/src/app/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+  private _userIsAuthenticated = false;
+  get userIsAuthentcated() {
+    return this._userIsAuthenticated;
+  }
+
+  login() {
+    this._userIsAuthenticated = true;
+  }
+  logout() {
+    this._userIsAuthenticated = false;
+  }
+}
