4da49e813d2a76e211e6a79b43758ec7950c269d
Nithin Samuel <nithin.thewebdev@gmail.com>
2025-04-26 09:26:07 +0530

    added ionic sliding items

--- Max Course/ionic/ionic-courses-project/src/app/places/offers/offers.page.ts
+++ Max Course/ionic/ionic-courses-project/src/app/places/offers/offers.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IonItemSliding, NavController } from '@ionic/angular';
 import { Place } from 'src/app/models/place.model';
 import { PlacesService } from '../places.service';
 
@@ -15,10 +15,16 @@ export class OffersPage implements OnInit {
   offerrs: Place[] = [];
 
 
-  constructor(private route: ActivatedRoute, private navCtrl: NavController, private placesService: PlacesService) { }
+  constructor(private route: ActivatedRoute, private navCtrl: NavController, private placesService: PlacesService, private router: Router) { }
 
   ngOnInit() {
     this.offerrs = this.placesService.places;
   }
-
+  onDeleteOffer(offerId: string) {
+    console.log("deletye offer");
+  }
+  onEditOffer(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
+  }
 }
