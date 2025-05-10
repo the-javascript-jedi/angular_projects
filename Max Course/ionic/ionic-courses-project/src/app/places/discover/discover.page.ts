35dad6ff6b6d7eda18e5d26c40ded7b3110b9c0d
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-05-10 22:53:13 +0530

    added segments for navigation

--- Max Course/ionic/ionic-courses-project/src/app/places/discover/discover.page.ts
+++ Max Course/ionic/ionic-courses-project/src/app/places/discover/discover.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Place } from 'src/app/models/place.model';
 import { PlacesService } from '../places.service';
-import { MenuController } from '@ionic/angular';
+import { MenuController, SegmentChangeEventDetail } from '@ionic/angular';
 
 @Component({
   selector: 'app-discover',
@@ -18,5 +18,24 @@ export class DiscoverPage implements OnInit {
   }
   onOpenMenu() {
     this.menuCtrl.toggle();
+  }
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>|string) {
+    if (typeof event === 'string') {
+      // If it's a string, use it directly
+      console.log(event);
+      // Filter using the string value
+      // this.loadedPlaces = this.placesService.places.filter(place => {
+      //   return place.title.toLowerCase().includes(event.toLowerCase());
+      // });
+    } else {
+      // If it's an event, access the value property safely
+      console.log(event);
+      const value = event.detail?.value;
+      console.log('Value:', value);
+      // Filter using the event value
+      // this.loadedPlaces = this.placesService.places.filter(place => {
+      //   return place.title.toLowerCase().includes(value.toLowerCase());
+      // });
+    }
   }
 }
