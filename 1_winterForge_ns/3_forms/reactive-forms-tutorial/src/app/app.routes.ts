fed154473546f780c77eb6003f08b358591b36ad
nithin.thewebdev <nithin.thewebdev@gmail.com>
2026-06-25 11:34:56 +0530

    ladder two

--- 1_winterForge_ns/3_forms/reactive-forms-tutorial/src/app/app.routes.ts
+++ 1_winterForge_ns/3_forms/reactive-forms-tutorial/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LadderOne } from './ladders/ladder-one/ladder-one';
+import { LadderTwo } from './ladders/ladder-two/ladder-two';
 
 export const routes: Routes = [
   { path: 'loginForm', component: LadderOne },
+  { path: 'signupForm', component: LadderTwo },
 ];
---
+++ 1_winterForge_ns/3_forms/reactive-forms-tutorial/src/app/ladders/ladder-two/ladder-two.ts
@@ -0,0 +1,37 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { passwordMatchValidator } from './passwordMatchValidator';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ladder-two',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './ladder-two.html',
+  styleUrl: './ladder-two.scss',
+})
+export class LadderTwo {
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group(
+    {
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(3)]],
+      confirmPassword: ['', Validators.required],
+    },
+    { validators: passwordMatchValidator },
+  );
+
+  get email() {
+    return this.form.get('email');
+  }
+  get password() {
+    return this.form.get('password');
+  }
+  get confirmPassword() {
+    return this.form.get('confirmPassword');
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+}
