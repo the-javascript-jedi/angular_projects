d12b521d79b3e9a60b2bf40eac1ca586f8f67321
nithin.thewebdev <nithin.thewebdev@gmail.com>
2026-07-01 09:19:58 +0530

    added form group

--- 1_winterForge_ns/3_forms/reactive-forms-tutorial/src/app/app.routes.ts
+++ 1_winterForge_ns/3_forms/reactive-forms-tutorial/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { LadderOne } from './ladders/ladder-one/ladder-one';
 import { LadderTwo } from './ladders/ladder-two/ladder-two';
 import { LadderThree } from './ladders/ladder-three/ladder-three';
+import { LadderFour } from './ladders/ladder-four/ladder-four';
 
 export const routes: Routes = [
   { path: 'loginForm', component: LadderOne },
   { path: 'signupForm', component: LadderTwo },
   { path: 'userNameAvailability', component: LadderThree },
+  { path: 'userNameAvailability', component: LadderThree },
+  { path: 'dynamicForm', component: LadderFour },
 ];
